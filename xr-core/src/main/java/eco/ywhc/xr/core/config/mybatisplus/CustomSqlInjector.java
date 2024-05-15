5e88d104777dad51a5e804a86a8169476983db75
申达慧 <shendahui@gmail.com>
2024-05-15 17:51:47 +0800

    回退mybatis-plus版本到3.5.5

--- xr-core/src/main/java/eco/ywhc/xr/core/config/mybatisplus/CustomSqlInjector.java
+++ xr-core/src/main/java/eco/ywhc/xr/core/config/mybatisplus/CustomSqlInjector.java
@@ -4,15 +4,14 @@ import com.baomidou.mybatisplus.core.injector.AbstractMethod;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
 import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn;
-import org.apache.ibatis.session.Configuration;
 
 import java.util.List;
 
 public class CustomSqlInjector extends DefaultSqlInjector {
 
     @Override
-    public List<AbstractMethod> getMethodList(Configuration configuration, Class<?> mapperClass, TableInfo tableInfo) {
-        List<AbstractMethod> methodList = super.getMethodList(configuration, mapperClass, tableInfo);
+    public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+        List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
         methodList.add(new InsertBatchSomeColumn("bulkInsert", i -> !i.getColumn().isBlank()));
         return methodList;
     }
