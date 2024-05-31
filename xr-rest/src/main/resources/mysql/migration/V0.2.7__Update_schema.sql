26130f3904e43ed12c43158a60a31f225db788f2
梁爽 <shuang.liang@lingycloud.com>
2024-05-31 10:39:43 +0800

    修复V0.2.7__Update_schema.sql可能执行失败的问题

--- xr-rest/src/main/resources/mysql/migration/V0.2.7__Update_schema.sql
+++ xr-rest/src/main/resources/mysql/migration/V0.2.7__Update_schema.sql
@@ -1,5 +1,6 @@
 ALTER TABLE `b_task_template`
-    CHANGE COLUMN `department_id` `instance_role_id` bigint NOT NULL COMMENT '实例角色ID' AFTER `id`;
+    DROP COLUMN `department_id`,
+    ADD COLUMN `instance_role_id` bigint NOT NULL COMMENT '实例角色ID' AFTER `id`;
 
 ALTER TABLE `b_task`
     DROP COLUMN `department`,
